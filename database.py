ca3da91fe75658a7b6c59263f39a0f9cbd5e71f5
Eugene Ashong <geniedatt@yahoo.com>
2026-03-09 19:54:16 -0700

    fix sqlite concurrency for streamlit

--- database.py
+++ database.py
@@ -1,12 +1,6 @@
 import sqlite3
 
-
-def get_connection():
-    return sqlite3.connect("tasks.db", check_same_thread=False)
-
-
-# create table
-conn = get_connection()
+conn = sqlite3.connect("tasks.db", check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute("""
@@ -17,108 +11,59 @@ CREATE TABLE IF NOT EXISTS tasks (
     streak INTEGER DEFAULT 0
 )
 """)
-
 conn.commit()
-conn.close()
 
 
 def add_task(task):
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
     clean_task = task.strip().lower()
 
-    cursor.execute(
-        "SELECT * FROM tasks WHERE LOWER(task)=?",
-        (clean_task,)
-    )
-
+    cursor.execute("SELECT * FROM tasks WHERE LOWER(task)=?", (clean_task,))
     exists = cursor.fetchone()
 
     if not exists:
-        cursor.execute(
-            "INSERT INTO tasks (task) VALUES (?)",
-            (clean_task,)
-        )
+        cursor.execute("INSERT INTO tasks (task) VALUES (?)", (clean_task,))
         conn.commit()
-
-    conn.close()
 
 
 def get_tasks():
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT id, task, completed FROM tasks"
-    )
-
-    tasks = cursor.fetchall()
-
-    conn.close()
-
-    return tasks
+    cursor.execute("SELECT id, task, completed FROM tasks")
+    return cursor.fetchall()
 
 
 def complete_task(task_id):
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "UPDATE tasks SET completed=1 WHERE id=?",
-        (task_id,)
-    )
-
+    cursor.execute("UPDATE tasks SET completed=1 WHERE id=?", (task_id,))
     conn.commit()
-    conn.close()
-
 
 def delete_task(task_id):
-
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "DELETE FROM tasks WHERE id=?",
-        (task_id,)
-    )
-
+    cursor.execute("DELETE FROM tasks WHERE id=?", (task_id,))
     conn.commit()
-    conn.close()
 
 
 def update_streak(task_id):
 
-    conn = get_connection()
-    cursor = conn.cursor()
-
     cursor.execute(
-        "UPDATE tasks SET streak = streak + 1 WHERE id=?",
+        "UPDATE tasks SET streak = streak + 1 WHERE id = ?",
         (task_id,)
     )
 
     conn.commit()
-    conn.close()
-
 
 def get_streak(task_id):
 
-    conn = get_connection()
-    cursor = conn.cursor()
-
     cursor.execute(
-        "SELECT streak FROM tasks WHERE id=?",
+        "SELECT streak FROM tasks WHERE id = ?",
         (task_id,)
     )
 
     result = cursor.fetchone()
 
-    conn.close()
-
     if result:
         return result[0]
 
     return 0
+
+
+
+
+
+
